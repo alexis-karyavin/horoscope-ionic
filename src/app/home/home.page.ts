574824c83518383f3071f4abc10e7a5189d26fc7
alexis-karyavin <alexis-karyavin@yandex.ru>
2020-04-28 22:17:49 +0300

    complete app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import data from '../../assets/zodiacSings.json';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  public data = data;
+  public description: string;
   constructor() {}
 
+  public changeZodiac(event): void {
+    const key = event.detail.value;
+    const id = this.data.findIndex(item => {
+      return item.name === key;
+    });
+    this.description = this.data[id].description;
+  }
 }
